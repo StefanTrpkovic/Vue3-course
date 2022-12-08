f20e8c175c4aa67f5b79acafac9f47c1853e3c61
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-12-08 16:57:25 +0100

    End of episode 8

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -17,6 +17,13 @@ export default {
         :assignments=filters.completed
       >
       </assignment-list>
+
+      <form @submit.prevent="add">
+        <div class="border border-gray-600 text-black">
+          <input v-model="newAssignment" placeholder="New Assignment..." class="p-2"/>
+          <button type="submit" class="bg-white p-2 border-l">Add</button>
+        </div>
+      </form>
     </section>  
   `,
 
@@ -26,16 +33,23 @@ export default {
                 { name: 'Finish project', complete: false, id: 1 },
                 { name: 'Read Chapter 4', complete: false, id: 2 },
                 { name: 'Turn in Homework', complete: false, id: 3 },
-            ]
+            ],
+            newAssignment: null
         }
     },
-
-      computed: {
-        filters() {
-          return {
-            inProgress: this.assignments.filter(assignment => ! assignment.complete),
-            completed: this.assignments.filter(assignment => assignment.complete)
-          }
+    computed: {
+      filters() {
+        return {
+          inProgress: this.assignments.filter(assignment => ! assignment.complete),
+          completed: this.assignments.filter(assignment => assignment.complete)
         }
+      }
+    },
+    methods: {
+      add() {
+        const highestNumber = Math.max(...this.assignments.map(assignment => assignment.id))
+        this.newAssignment && this.assignments.push({ name: this.newAssignment, complete: false, id: highestNumber + 1 })
+        this.newAssignment = null
+      }
     }
 }
