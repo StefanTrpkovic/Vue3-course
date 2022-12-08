a0163a34f7e7a2b7d55ffb2a8e0562d9221ef3d6
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-12-08 18:59:51 +0100

    End od episode 10

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -6,11 +6,24 @@ export default {
   },
   template: `
       <section v-show="assignments.length">
-      <h2 class="font-bold mb-2">{{title}}</h2>
+      <h2 class="font-bold mb-2">{{title}} ({{assignments.length}})</h2>     
+      
+      <div class="flex gap-2">
+        <button 
+        v-for="tag in tags" 
+        class="border rounded px-1 py-px text-xs" 
+        :class="{
+          'border-blue-500 text-blue-500': tag === currentTag
+        }"
+        @click="currentTag = tag"
+      >
+        {{ tag }}
+      </button>
+      </div>
 
-      <ul class="border border-gray-600 divide-y divide-gray-600">
+      <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
          <Assignment 
-          v-for="assignment in assignments"
+          v-for="assignment in filteredAssignments"
           :key="assignment.id"
           :assignment="assignment"
          >
@@ -21,5 +34,20 @@ export default {
   props: {
     assignments: Array,
     title: String
-  }
+  },
+  computed: {
+    tags() {
+      return ['all', ...new Set(this.assignments.map(x=>x.tag))]
+    },
+    filteredAssignments() {
+      if(this.currentTag === 'all') return this.assignments
+
+      return this.assignments.filter(x=>x.tag === this.currentTag);
+    }
+  },
+  data() {
+    return {
+      currentTag: "all"
+    }
+  },
 }
